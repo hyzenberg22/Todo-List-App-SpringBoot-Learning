ceb1d5fd0a7c1f663458602742497f99fa5a9756
hyzenberg22 <hyzenberg22@protonmail.com>
2023-06-05 14:22:40 +0530

    Password Encoder implemented

---
+++ src/main/java/com/in28minutes/springboot/Myfirstapplication/security/SpringSecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.in28minutes.springboot.Myfirstapplication.security;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SpringSecurityConfiguration {
+	//lDAP or Database 
+	//In Memory 
+	
+//	InMemoryUserDetailsManager
+//	inMemoryUserDetailsManager(UserDetails... users)
+	
+	@Bean
+	public InMemoryUserDetailsManager createUserDetailsManager() {
+		Function<String, String> passwordEncoder;
+		UserDetails userDetails=User.builder()
+			.passwordEncoder(passwordEncoder)	
+			.username("test")
+			.password("test")
+			.roles("USER", "ADMIN")
+			.build();
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+}
